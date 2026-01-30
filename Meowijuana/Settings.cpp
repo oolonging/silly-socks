1d06d4ccf805ba71d08846ec2f0d35aa2e0813c5
Saahil <29069313+SALTx@users.noreply.github.com>
2026-01-30 11:55:03 +0800

    Added settings for later

---
+++ Meowijuana/Settings.cpp
@@ -0,0 +1,5 @@
+
+#include "AEEngine.h"
+#include "Settings.hpp"
+
+namespace Settings {};
---
+++ Meowijuana/Settings.hpp
@@ -0,0 +1,15 @@
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+
+#include "Graphics.hpp"
+
+namespace Settings {
+	// colors
+	static Color::CL_Color gFillColor{};
+	static Color::CL_Color gStrokeColor{};
+
+	// 
+}
+
+
+#endif // SETTINGS_HPP
