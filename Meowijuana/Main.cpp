212bf5e5c19204bfa2ff4fa4953f60b152f77f56
Saahil <29069313+SALTx@users.noreply.github.com>
2026-02-04 00:55:59 +0800

    Update Main.cpp

--- Meowijuana/Main.cpp
+++ Meowijuana/Main.cpp
@@ -9,6 +9,7 @@
 #include "GameStateManager.hpp"
 #include "Entity.hpp"
 #include "World.hpp"
+#include "Settings.hpp"
 
 extern int current, previous, next;
 extern FP fpLoad, fpInitialize, fpUpdate, fpDraw, fpFree, fpUnload;
@@ -46,6 +47,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	World::initGrid(AEGfxGetWindowWidth(), AEGfxGetWindowHeight(), 100);
 
+	AEGfxTexture* dungeonTile = AEGfxTextureLoad("Assets/Tiles/DUNGEON_TILE.png");
+
 	GSM_Initialize(GS_LEVEL1);
 
 	while (current != GS_QUIT)
@@ -62,13 +65,22 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		}
 
 		fpInitialize();
-
+		Color::noStroke();
 		// -------- LEVEL LOOP --------
 		while (next == current)
 		{
 			AESysFrameStart();
 
 			Color::background(Color::CL_Color_Create(255, 255, 0));
+			//Graphics::image(-800.0f, 450.0f, 1600.0f, 900.0f, dungeonTile, Shapes::CORNER);
+
+			// try tiling it
+			Color::fill(255, 255, 255);
+			for (int i = 0; i < 16; i++) {
+				for(int j = 0; j < 9; j++) {
+					Graphics::image(-800.0f + i * 100.0f, 450.0f - j * 100.0f, 100.0f, 100.0f, dungeonTile, Shapes::CORNER);
+				}
+			}
 
 			if (AEInputCheckTriggered(AEVK_ESCAPE) ||
 				0 == AESysDoesWindowExist())
