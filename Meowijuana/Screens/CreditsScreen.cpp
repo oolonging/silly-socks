3551675a291b7360a0e63a19c93f3892fc049654
smikeywikey <smikeywikey@gmail.com>
2026-03-07 23:00:25 +0800

    simple credits

--- Meowijuana/Screens/CreditsScreen.cpp
+++ Meowijuana/Screens/CreditsScreen.cpp
@@ -3,11 +3,29 @@
 #include "../Graphics.hpp"
 #include "CreditsScreen.hpp"
 
+//global variables for credits screen
+float scrollY;
+float scrollSpeed;
+const char* credits[] = {
+	"Thank you for playing!",
+	"Yu Xuan",
+	"Saahil",
+	"Jun",
+	"Andrea",
+	"Silly Socks",
+};
+
+int creditsCount = sizeof(credits) / sizeof(credits[0]);
+
 void Credits_Load() {
 
 }
 
 void Credits_Initialize() {
+
+	scrollY = -AEGfxGetWindowHeight();
+	scrollSpeed = 100.0f;
+
 
 }
 
@@ -16,11 +34,18 @@ void Credits_Update() {
 	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
 	Color::textFill(255, 255, 255);
 	Color::background({ 0, 0, 0, 255 });
-	Text::text("Credits screen", 0, 0);
+
+	//move upward
+	scrollY += scrollSpeed * static_cast<float>(AEFrameRateControllerGetFrameTime());
+
 }
 
 void Credits_Draw() {
-
+	// Draw each line
+	for (int i = 0; i < creditsCount; i++) {
+		float yPos = scrollY + i * 100; //spacing between lines
+		Text::text(credits[i], 0, yPos);
+	}
 }
 
 void Credits_Free() {
