09902f0ec273e93ed9c1a0f862a757fb94d0008d
Lau Yu Xuan <yux201932@gmail.com>
2026-03-17 20:16:09 +0800

    updating tutorialdungeon
    todo: list in tutorialdungeon comments

--- Meowijuana/TutorialDungeon.cpp
+++ Meowijuana/TutorialDungeon.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include "AEEngine.h"
+#include <iostream>
 #include "TutorialDungeon.hpp"
 #include "../../Graphics.hpp"
 #include "../../Tiles.hpp"
@@ -7,23 +8,55 @@
 #include "../../DungeonManager.hpp"
 #include "../../Managers/EntityManager.hpp"
 #include "../../World.hpp"
+#include "../../GameStateManager.hpp"
+
 
 World::worldGrid TDungeonGrid;
 std::pair<int, int> tutActiveTile;
 int tutorialRoomIndex = 0;
 
-Entity::Player tutPlayer;
-Entity::Enemy tutEnemy1, tutEnemy2, tutEnemy3;
 
+//  TODO : CHARACTER DEATH, MAKE ROOMS MORE INTERESTING, ENTITY COLLISION WITH WALL, PARTICLES IF POSSIBLE, KNOCKBACK???????
+
+
+// ------------- ROOM STATE (similar to the non-hard coded dungeonsS) ----------------------------'
+struct TutorialRoomState {
+
+    bool visited = false;
+    bool cleared = false;
+
+};
+
+TutorialRoomState tutorialRooms[4];
 
 // placeholder til teleporter comes in
 void LoadRoom(int roomIndex) {
     if (roomIndex == 1)
         TDungeonGrid.fillGrid("Assets/LevelMaps/TutorialDungeon/sideRoomL1_11.txt");
     else if (roomIndex == 2)
-        TDungeonGrid.fillGrid("Assets/LevelMaps/TutorialDungeon/sideRoomL1_12.txt");
+        TDungeonGrid.fillGrid("Assets/LevelMaps/TutorialDungeon/sideRoomL1_21.txt");
+    else if (roomIndex == 3)
+        TDungeonGrid.fillGrid("Assets/LevelMaps/TutorialDungeon/sideRoomL1_33.txt");
     else
         TDungeonGrid.fillGrid("Assets/LevelMaps/TutorialDungeon/mainRoomL1.txt");
+
+
+    if (!tutorialRooms[roomIndex].visited) {
+        tutorialRooms[roomIndex].visited = true;
+
+        if (roomIndex != 0) {
+            EntityManager::spawnEnemies(2, 800.0f, 600.0f);
+
+            Inventory::Weapon* eWeapon = dynamic_cast<Inventory::Weapon*>(Inventory::ItemRegistry::createItem(Inventory::ItemID::WOODEN_SWORD));
+            if (eWeapon) {
+                EntityManager::weaponEnemies(eWeapon);
+                std::cout << "has weapon";
+            }
+        }
+    }
+
+
+
 }
 
 
@@ -35,103 +68,112 @@ void TutorialDungeon_Initialize() {
     TDungeonGrid.initGrid(AEGfxGetWindowWidth(), AEGfxGetWindowHeight(), 50);
     TDungeonGrid.initMapTexture();
     TDungeonGrid.initTextureBox();
-    LoadRoom(tutorialRoomIndex);
 
-    tutPlayer = Entity::Player(-600.0f, 50.0f, 50.0f, 50.0f, 100.0f, 5.0f, 0.0f);
-    tutEnemy1 = Entity::Enemy(-100.0f, 50.0f, 50.0f, 50.0f, 100.0f, 2.0f, 0.0f);
-    tutEnemy2 = Entity::Enemy(100.0f, 50.0f, 50.0f, 50.0f, 100.0f, 2.0f, 0.0f);
-    tutEnemy3 = Entity::Enemy(200.0f, 50.0f, 50.0f, 50.0f, 100.0f, 2.0f, 0.0f);
-
+    EntityManager::init();
+    auto* tutPlayer = EntityManager::getPlayer("player");
 
     Inventory::init();
     Inventory::Weapon* pWeapon = dynamic_cast<Inventory::Weapon*>(Inventory::ItemRegistry::createItem(Inventory::ItemID::CARROT_SWORD));
     if (pWeapon) {
-        tutPlayer.setWeapon(pWeapon);
-        tutPlayer.setAtkSpd(pWeapon->getAttackSpeed());
+        tutPlayer->setWeapon(pWeapon);
+        tutPlayer->setAtkSpd(pWeapon->getAttackSpeed());
     }
 
-    Inventory::Weapon* eWeapon = dynamic_cast<Inventory::Weapon*>(Inventory::ItemRegistry::createItem(Inventory::ItemID::WOODEN_SWORD));
-    if (eWeapon) {
-        tutEnemy1.setWeapon(eWeapon);
-        tutEnemy2.setWeapon(eWeapon);
-        tutEnemy3.setWeapon(eWeapon);
-    }
+    //Inventory::Weapon* eWeapon = dynamic_cast<Inventory::Weapon*>(Inventory::ItemRegistry::createItem(Inventory::ItemID::WOODEN_SWORD));
+    //if (eWeapon) {
+    //    EntityManager::weaponEnemies(eWeapon);
+    //    std::cout << "has weapon";
+    //}
+
+    LoadRoom(tutorialRoomIndex);
 }
 
 
 
 void TutorialDungeon_Update() {
-    tutActiveTile = World::activeTile(tutPlayer.getX(), tutPlayer.getY(), TDungeonGrid);
-    /*if (AEInputCheckTriggered(AEVK_E))
-        World::interactTile(tutActiveTile, TDungeonGrid);*/
+    auto* tutPlayer = EntityManager::getPlayer("player");
 
-    tutPlayer.update();
-    tutPlayer.tickAttackTimer();
+    tutActiveTile = World::activeTile(tutPlayer->getX(), tutPlayer->getY(), TDungeonGrid);
 
-    Entity::Enemy* enemies[] = { &tutEnemy1, &tutEnemy2, &tutEnemy3 };
+    tutPlayer->update(TDungeonGrid);
+    tutPlayer->tickAttackTimer();
 
-    for (auto* enemy : enemies) {
-        if (!enemy->isAlive()) continue;
-        enemy->tickAttackTimer();
-        if (enemy->canAttack()) {
-            enemy->attack(tutPlayer);
-            enemy->resetAttackTimer();
-        }
+    EntityManager::updateEnemies(*tutPlayer);
+
+
+    if (AEInputCheckTriggered(AEVK_LBUTTON) && tutPlayer->canAttack()) {
+        EntityManager::attackEnemies(*tutPlayer);
+        tutPlayer->resetAttackTimer(); // reset once after hitting all enemies
     }
 
 
-    if (AEInputCheckTriggered(AEVK_LBUTTON) && tutPlayer.canAttack()) {
-        for (auto* enemy : enemies) {
-            if (enemy->isAlive()) {
-                tutPlayer.attack(*enemy);
-                if (enemy->getHp() <= 0) enemy->setHp(0);
-            }
-        }
-        tutPlayer.resetAttackTimer(); // reset once after hitting all enemies
+    if (!tutorialRooms[tutorialRoomIndex].cleared && EntityManager::allEnemiesDead()){
+        tutorialRooms[tutorialRoomIndex].cleared = true;
     }
 
+    if (tutPlayer->getHp() <= 0) tutPlayer->setHp(0);
 
-
-    if (tutPlayer.getHp() <= 0) tutPlayer.setHp(0);
+    
 
     float halfHeight = AEGfxGetWindowHeight() / 2.0f;
-    float savedX = tutPlayer.getX();
+    float halfWidth = AEGfxGetWindowWidth() / 2.0f;
+    float savedX = tutPlayer->getX();
+    float savedY = tutPlayer->getY();
 
-    if (tutPlayer.getY() >= halfHeight && tutorialRoomIndex < 2) {
-        tutorialRoomIndex++;
-        TutorialDungeon_Initialize();
-        tutPlayer.setPosition(savedX, -halfHeight + 10.0f);
-    }
+    if (tutorialRooms[tutorialRoomIndex].cleared && EntityManager::allEnemiesDead()) {
 
-    else if (tutPlayer.getY() <= -halfHeight && tutorialRoomIndex > 0) {
-        tutorialRoomIndex--;
-        TutorialDungeon_Initialize();
-        tutPlayer.setPosition(savedX, halfHeight - 10.0f);
+        if (tutPlayer->getY() >= halfHeight && tutorialRoomIndex < 3) {
+            tutorialRoomIndex++;
+
+            LoadRoom(tutorialRoomIndex);
+            //auto* tutPlayer = EntityManager::getPlayer("player");
+            tutPlayer->setPosition(savedX, -halfHeight + 10.0f);
+        }
+
+        else if (tutPlayer->getY() <= -halfHeight && tutorialRoomIndex > 0) {
+            tutorialRoomIndex--;
+            LoadRoom(tutorialRoomIndex);
+            tutPlayer->setPosition(savedX, halfHeight - 10.0f);
+        }
+
+        if (tutPlayer->getX() >= halfWidth && tutorialRoomIndex < 3) {
+            tutorialRoomIndex--;
+            LoadRoom(tutorialRoomIndex);
+            tutPlayer->setPosition(-halfWidth + 10.0f, savedY);
+        }
+
+        else if (tutPlayer->getX() <= -halfWidth && tutorialRoomIndex > 0) {
+            tutorialRoomIndex++;
+            LoadRoom(tutorialRoomIndex);
+            tutPlayer->setPosition(halfWidth - 10.0f, savedY);
+        }
+
+        if (tutorialRooms[3].cleared) {
+            World::standOnTile(next, *tutPlayer, TDungeonGrid, GS_A);
+        }
     }
 }
 
 
 void TutorialDungeon_Draw() {
+    auto* tutPlayer = EntityManager::getPlayer("player");
     TDungeonGrid.drawTexture(TDungeonGrid);
     World::drawTile(tutActiveTile, TDungeonGrid);
 
     // debug: show weapon attack range
     float attackRange = 170; // i put 170 since i put it as i believe 85 as range?
 
-    circle(tutPlayer.getX(), tutPlayer.getY(), attackRange, Shapes::CENTER);
+    circle(tutPlayer->getX(), tutPlayer->getY(), attackRange, Shapes::CENTER);
 
-    tutPlayer.draw();
+    tutPlayer->draw();
 
-    Entity::Enemy* enemies[] = { &tutEnemy1, &tutEnemy2, &tutEnemy3 };
-    for (auto* enemy : enemies) {
-        if (enemy->isAlive())
-            enemy->draw(tutPlayer);
-    }
+    EntityManager::drawEnemies(*tutPlayer);
 }
 
 void TutorialDungeon_Free() {
     TDungeonGrid.unloadMapTexture();
     World::freeGrid();
+    EntityManager::clearEnemies();
 }
 
 void TutorialDungeon_Unload() {
