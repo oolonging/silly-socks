13c6d68c3889338da7d5dda12658bf533d6bd709
Saahil <29069313+SALTx@users.noreply.github.com>
2026-03-18 20:12:36 +0800

    Add files for level 1

---
+++ Meowijuana/Screens/Dungeon/level1.cpp
@@ -0,0 +1,13 @@
+#include "level1.hpp"
+
+void dungeonLevel1_Load(void) {}
+
+void dungeonLevel1_Initialize(void) {}
+
+void dungeonLevel1_Update(void) {}
+
+void dungeonLevel1_Draw(void) {}
+
+void dungeonLevel1_Free(void) {}
+
+void dungeonLevel1_Unload(void) {}
---
+++ Meowijuana/Screens/Dungeon/level1.hpp
@@ -0,0 +1,13 @@
+#ifndef DUNGEON_LEVEL1_HPP
+#define DUNGEON_LEVEL1_HPP
+
+#include "../../pch.h"
+
+void dungeonLevel1_Load(void);
+void dungeonLevel1_Initialize(void);
+void dungeonLevel1_Update(void);
+void dungeonLevel1_Draw(void);
+void dungeonLevel1_Free(void);
+void dungeonLevel1_Unload(void);
+
+#endif // DUNGEON_LEVEL1_HPP
