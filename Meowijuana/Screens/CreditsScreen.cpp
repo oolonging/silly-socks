79993696f6b75f092d29bb341a12206b19aef2eb
Saahil <29069313+SALTx@users.noreply.github.com>
2026-03-18 22:47:25 +0800

    Update CreditsScreen.cpp

--- Meowijuana/Screens/CreditsScreen.cpp
+++ Meowijuana/Screens/CreditsScreen.cpp
@@ -3,10 +3,14 @@
 
 #include "../Graphics.hpp"
 #include "../Settings.hpp"
+#include "../GameStateManager.hpp"
 
 //global variables for credits screen
 float scrollY;
 float scrollSpeed;
+
+s32 mouseWheelDelta{};
+
 const char* credits[] = {
 	"Thank you for playing!",
 	" ",
@@ -28,17 +32,21 @@ void Credits_Load() {
 }
 
 void Credits_Initialize() {
-
 	scrollY = -(AEGfxGetWindowHeight() * 0.5f) - (creditsCount * 100.0f);
 	scrollSpeed = 100.0f;
-
-
 }
 
 void Credits_Update() {
-
 	//move upward
 	scrollY += scrollSpeed * static_cast<float>(AEFrameRateControllerGetFrameTime());
+
+	// if the mouse wheel is being scrolled, change the speed
+	if (mouseWheelDelta > 0) scrollSpeed = -400.0f;
+	if (mouseWheelDelta < 0) scrollSpeed = 400.0f;
+	if (mouseWheelDelta == 0) scrollSpeed = 100.0f;
+
+	// get mousewheel delta
+	AEInputMouseWheelDelta(&mouseWheelDelta);
 
 }
 
@@ -54,14 +62,15 @@ void Credits_Draw() {
 		float yPos = scrollY + i * 100; //spacing between lines
 		Text::text(credits[i], 0, yPos);
 	}
+
+	// Click anywhere to return to main menu
+	if (AEInputCheckTriggered(AEVK_LBUTTON) || AEInputCheckTriggered(AEVK_E)) {
+		next = GS_MAIN_MENU;
+	}
 }
 
-void Credits_Free() {
-
-}
-
-void Credits_Unload() {
-
-}
+// No assets to clear out yet. TODO: potentially add some images
+void Credits_Free() {}
+void Credits_Unload() {}
 
 
