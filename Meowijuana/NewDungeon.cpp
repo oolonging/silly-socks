c5278d7da7ecbeebf6b6bae171cd109f9c93d159
Saahil <29069313+SALTx@users.noreply.github.com>
2026-03-25 09:09:25 +0800

    Update NewDungeon.cpp

--- Meowijuana/NewDungeon.cpp
+++ Meowijuana/NewDungeon.cpp
@@ -54,7 +54,7 @@ void NewDungeon_Initialize() {
 	inv.loadInventory(localPlayer, gameData);
 
 	// spawn 3 enemies
-	if(!newDungeonState.visited)
+	if(!newDungeonState.cleared)
 	EntityManager::spawnEnemies(2, 400.0f, 200.0f, 1.5f);
 
 	// give the player a weapon
